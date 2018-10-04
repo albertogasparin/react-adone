2e79e29f225f8915528602945a424a44073d80cd
Alberto Gasparin <albertogasparin@gmail.com>
2018-10-04 23:10:40 +1000

    Prepare for publication

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,11 @@
 'use strict';
 
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-flow'],
+  presets: [
+    ['@babel/preset-env', { modules: false }],
+    '@babel/preset-react',
+    '@babel/preset-flow',
+  ],
   plugins: [
     ['@babel/plugin-proposal-class-properties', { loose: false }],
     '@babel/plugin-proposal-export-namespace-from',
