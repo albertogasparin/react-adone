4d830fcb9cd4482daa97db2167a30d6f391d9f63
Alberto Gasparin <albertogasparin@gmail.com>
2018-10-20 00:25:35 +1100

    Improved Redux devtools connection

--- src/bind-actions.js
+++ src/bind-actions.js
@@ -2,9 +2,7 @@ export default function bindActions(actions, store, extraArg) {
   return Object.keys(actions).reduce((acc, k) => {
     // Using a custom produce so we can name fn for better debuggability
     const namedProduce = fn => {
-      Object.defineProperty(fn, 'name', {
-        value: k + (fn.name ? `.${fn.name}` : ''),
-      });
+      fn.displayName = fn.displayName || k + (fn.name ? `.${fn.name}` : '');
       store.produce(fn);
     };
     acc[k] = (...args) =>
--- src/enhancers/devtools.js
+++ src/enhancers/devtools.js
@@ -1,22 +1,30 @@
 import defaults from '../defaults';
 
+const connectDevTools = store => {
+  const devTools = window.__REDUX_DEVTOOLS_EXTENSION__.connect({
+    name: `Basket: ${store.key}`,
+  });
+  devTools.init(store.getState());
+  devTools.subscribe(message => {
+    if (message.type === 'DISPATCH') {
+      store.setState(JSON.parse(message.state));
+    }
+  });
+  return devTools;
+};
+
 const withDevtools = createStore => (...args) => {
   const store = createStore(...args);
 
   if (defaults.devtools && window && window.__REDUX_DEVTOOLS_EXTENSION__) {
     const origProduce = store.produce;
-    const devTools = window.__REDUX_DEVTOOLS_EXTENSION__.connect({
-      name: `Basket: ${store.key}`,
-    });
-    devTools.init(store.getState());
-    devTools.subscribe(message => {
-      if (message.type === 'DISPATCH') {
-        store.setState(JSON.parse(message.state));
-      }
-    });
+    let devTools;
     const produce = fn => {
       const result = origProduce(fn);
-      devTools.send(fn.name, store.getState(), {}, store.key);
+      if (!devTools) {
+        devTools = connectDevTools(store);
+      }
+      devTools.send(fn.displayName, store.getState(), {}, store.key);
       return result;
     };
     store.produce = produce;
