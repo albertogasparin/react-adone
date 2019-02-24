f08743ef4252a522f51271c83bdcd935e36be006
Alberto Gasparin <albertogasparin@gmail.com>
2019-02-25 07:49:01 +1100

    Lazy evaluate store key on first access (#49)

--- src/store/create.js
+++ src/store/create.js
@@ -1,11 +1,19 @@
 import hash from '../utils/hash';
 
-export function createStore({ name = '', initialState, actions }) {
+function createKey(initialState, actions, name) {
   const src = !name
     ? Object.keys(actions).reduce((acc, k) => acc + actions[k].toString(), '')
     : '';
+  return [name, hash(src + JSON.stringify(initialState))].filter(Boolean);
+}
+
+export function createStore({ name = '', initialState, actions }) {
+  let key;
   return {
-    key: [name, hash(src + JSON.stringify(initialState))].filter(Boolean),
+    get key() {
+      // lazy evaluate key on first access
+      return key || (key = createKey(initialState, actions, name));
+    },
     initialState,
     actions,
   };
